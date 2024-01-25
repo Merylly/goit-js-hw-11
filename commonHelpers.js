e8f3890993b7e0a5efe4d499961958bc658a00c2
Merylly <142231027+Merylly@users.noreply.github.com>
2024-01-25 11:32:25 +0000

    Deploying to gh-pages from @ Merylly/goit-js-hw-11@a884ac2e8464222d403f8c887379440c2c667266 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,11 +1,11 @@
-import{S as p}from"./assets/vendor-b41a7778.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&s(i)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();const h="https://pixabay.com/api/",y="41929630-0ce2e4f7023522073d7143cb8",l={form:document.getElementById("form"),resultContainer:document.getElementById("result-container")};l.form.addEventListener("submit",g);function g(n){n.preventDefault();const r=n.currentTarget,o=r.elements.picture.value;L(o).then(s=>{const t=s.hits.map(({webformatURL:i,largeImageURL:a,tags:c,likes:u,views:f,comments:m,downloads:d})=>`<li class="gallery-item">
-            <a href="${a}">
-  <img src="${i}" alt="${c}"></a>
+import{i as u,S as f}from"./assets/vendor-46aac873.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const s of t.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&i(s)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();const m="https://pixabay.com/api/",d="41929630-0ce2e4f7023522073d7143cb8",a={form:document.getElementById("form"),resultContainer:document.getElementById("result-container")};a.form.addEventListener("submit",h);function h(n){n.preventDefault();const r=n.currentTarget,o=r.elements.picture.value;p(o).then(i=>{const e=i.hits;e.length===0&&u.error({message:"Sorry, there are no images matching your search query. Please try again!"}),g(e)}).finally(()=>r.reset())}function p(n){const r=new URLSearchParams({key:d,q:n,image_type:"photo",orientation:"horizontal",safesearch:!0,per_page:9});return fetch(`${m}?${r}`).then(o=>{if(!o.ok)throw new Error(o.statusText);return o.json()})}let y=new f(".gallery a",{captionsData:"alt",captionDelay:250});function g(n){const r=n.map(({webformatURL:o,largeImageURL:i,tags:e,likes:t,views:s,comments:l,downloads:c})=>`<li class="gallery-item">
+            <a href="${i}">
+  <img src="${o}" alt="${e}"></a>
   <ul>
-    <li>Likes: ${u}</li>
-    <li>Views: ${f}</li>
-    <li>Comments: ${m}</li>
-    <li>Downloads: ${d}</li>
+    <li>Likes: ${t}</li>
+    <li>Views: ${s}</li>
+    <li>Comments: ${l}</li>
+    <li>Downloads: ${c}</li>
   </ul>
-</li>`).join("");l.resultContainer.innerHTML=t}).finally(()=>r.reset())}function L(n){const r=new URLSearchParams({key:y,q:n,image_type:"photo",orientation:"horizontal",safesearch:!0,per_page:9});return fetch(`${h}?${r}`).then(o=>{if(!o.ok)throw new Error(o.statusText);return o.json()})}new p(".gallery a",{captionsData:"alt",captionPosition:"bottom",captionDelay:250});
+</li>`).join("");a.resultContainer.innerHTML=r,refresh(),y.refresh()}
 //# sourceMappingURL=commonHelpers.js.map
